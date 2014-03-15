2cbac0539859be1ccf2db5fec73f431418de8051
Gooi <samgooi4189@gmail.com>
2014-03-14 22:39:09 -0500

    Default configuration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MongoRails::Application.config.secret_key_base = '29b8bacca0a70a5c76ffc39a34e78842d3fa047ea03e8aa78e69dfba2849ed89d33f55d344641f68e1854e343c4910e223587bc94dbf9ece867406f9b7cf9965'
